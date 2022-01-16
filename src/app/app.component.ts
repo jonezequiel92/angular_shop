a052f2018461d58391e034141b44c5fa90774b59
Jonathan Ezequiel <jonezequiel@gmail.com>
2022-01-15 21:03:39 -0300

    NG-IF and NG-FOR test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'newProyect';
+
+  heroes = [
+    { id: 1, name: 'Superman' },
+    { id: 2, name: 'Batman' },
+    { id: 3, name: 'BatGirl' },
+    { id: 4, name: 'Robin' },
+    { id: 5, name: 'Flash' },
+  ];
+
+  addItem() {
+    this.heroes.push({ id: 6, name: 'nuevo item' });
+  }
+
+  deleteItem(index: number) {
+    this.heroes.splice(index, 1);
+  }
 }
